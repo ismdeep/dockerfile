09c2e670fb9b651a98edfbfdf7e10b53a781e9ab
ismdeep <ismdeep@protonmail.com>
2020-09-29 19:56:25 +0800

    Update

--- cloud9-docker/server-start.sh
+++ cloud9-docker/server-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /data/c9sdk && /data/node-v12.16.1-linux-x64/bin/node server.js --listen 0.0.0.0 --port 80 -w /workspace --auth admin:admin
+cd /data/c9sdk && /data/node-v12.18.4-linux-x64/bin/node server.js --listen 0.0.0.0 --port 80 -w /workspace --auth admin:admin
