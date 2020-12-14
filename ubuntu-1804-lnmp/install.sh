fac0f933732938a035502e0cb1082ffcf0916b62
L. Jiang <l.jiang.1024@gmail.com>
2020-12-14 23:00:48 +0800

    update ubuntu-1804-lnmp

--- ubuntu-1804-lnmp/install.sh
+++ ubuntu-1804-lnmp/install.sh
@@ -8,5 +8,5 @@ apt-get install -y nginx
 apt-get install -y mysql-server-5.7
 apt-get install -y php-fpm php-mysql php-zip php-xml php-gd php-mbstring
 apt-get install -y supervisor
-cd /root && curl -sS https://getcomposer.org/installer -o composer-setup.php && php composer-setup.php --install-dir=/usr/local/bin --filename=composer && rm composer-setup.php
+cd /root && curl -sSL https://github.com/composer/composer/releases/download/1.10.19/composer.phar -o composer && chmod +x composer && mv composer /usr/local/bin/composer
 locale-gen en_US.UTF-8
